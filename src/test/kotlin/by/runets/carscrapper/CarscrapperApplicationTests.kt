28d1a5d17661ed692fdbb55104d6ce196eaaacc5
krunets <Kiryl_Runets@epam.com>
2020-08-27 10:39:55 +0300

    Spotify docker plugin

--- src/test/kotlin/by/runets/carscrapper/CarscrapperApplicationTests.kt
+++ src/test/kotlin/by/runets/carscrapper/CarscrapperApplicationTests.kt
@@ -1,9 +1,7 @@
 package by.runets.carscrapper
 
-import by.runets.carscrapper.utils.UtilWebClient
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import java.io.File
 
 @SpringBootTest
 class CarscrapperApplicationTests {
